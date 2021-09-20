fa8291d965241d1e379415eaba18d4abafb328c2
Raj Kishan <68591610+rajkishancode@users.noreply.github.com>
2021-09-20 22:36:51 +0530

    A web app that tells you if your birthday is lucky or not.

---
+++ app.js
@@ -0,0 +1,43 @@
+const dateOfBirth = document.querySelector("#data-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box")
+
+
+function compareValues(sum, luckyNumber) {
+    if (sum % luckyNumber == 0){
+        outputBox.innerText = "Your birthday is lucky!!🤞";
+     
+
+    }else{
+        outputBox.innerText = "Your Birthday is not lucky!😢";
+    
+    }
+ 
+}
+
+function checkBirthdayDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if(dob&&sum)
+    compareValues(sum, luckyNumber.value);
+    else
+    outputBox.innerText = 'Please fill out all required fields.';
+
+}
+
+
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for(let index=0;index<dob.length;index++)
+    {
+    sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+    
+}
+
+checkNumberButton.addEventListener('click',checkBirthdayDateIsLucky)
+
